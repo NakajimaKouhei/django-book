2fedf1a3ea6318a2c6066b4f565c92112578e0d3
NakajimaKouhei <nakajima-kohei@ixui.co.jp>
2017-05-14 14:47:28 +0900

    #9
    ログイン処理部分を作成　残りログイン認証を独自実装

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
 ]
